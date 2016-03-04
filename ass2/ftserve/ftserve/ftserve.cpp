721791583f5023467d6dc7823a933cabc3e5ae0d
Dane Schoonover <Dane@Danes-iMac.local>
2016-03-03 17:57:04 -0800

    update

--- ass2/ftserve/ftserve/ftserve.cpp
+++ ass2/ftserve/ftserve/ftserve.cpp
@@ -6,14 +6,17 @@
 //  Copyright © 2016 dane. All rights reserved.
 
 
-#include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <dirent.h>
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fstream>
 using namespace std;
 
 //----------------------------------------------
@@ -48,16 +51,19 @@ int acceptConnection(int);
 // Takes a chatSocket
 char* receiveCommand(int);
 
-int createDataSocket(int);
+int createDataSocket(int, int);
 
 // Sends requested directory listing to client
 void listDirectory(int dataSocket);
 
 // Sends requested file to client
-void transferFile();
+void transferFile(int dataSocket, char*);
 
 // Utilize the above
-void handleRequest(int);
+void handleRequest(int, int);
+
+
+
 
 
 // MAIN --------------------------------------------------------------------------------
@@ -70,11 +76,13 @@ int main(int argc, const char * argv[]) {
     cout << "Server started" << endl;
     
     // Accept incoming connections and handle requests
-    handleRequest(serverSocket);
+    handleRequest(serverSocket, port);
     
     return 0;
 }
 // -------------------------------------------------------------------------------------
+
+
 // MAIN FUNCTIONS ----------------------------------------------------------------------
 
 int startup(int port){
@@ -85,7 +93,7 @@ int startup(int port){
     return serverSocket;
 }
 
-void handleRequest(int serverSocket){
+void handleRequest(int serverSocket, int port){
     while(1) {
         int dataSocket = 0;
         
@@ -96,19 +104,21 @@ void handleRequest(int serverSocket){
         char* command = receiveCommand(uniqueClient);
         
         // Create data socket for pass data to client (if command is sent)
-        if (command)
-            dataSocket = createDataSocket(uniqueClient);
+//        if (command)
+        dataSocket = createDataSocket(uniqueClient, port);
         
         // Send data based on command
         if (strcmp(command,"-l")==0)
             listDirectory(dataSocket);
         else
-            transferFile(dataSocket);
+            transferFile(dataSocket, command);
         
         free((char*)command);
     }
 }
 // -------------------------------------------------------------------------------------
+
+
 
 // INCOMING CONNECTION HANDLER ---------------------------------------------------------
 int acceptConnection(int socket){
@@ -126,22 +136,58 @@ char* receiveCommand(int serverSocket){
     return newMessage;
 };
 
-int createDataSocket(int uniqueClient){
+int createDataSocket(int uniqueClient, int port){
+    // New data socket for FTP
+    int dataSocket = 0;
     
+    // Dataport
+    int dataport = port - 1;
+    
+    // Assign client IP info to Socket
+    struct sockaddr_in ipInfo;
+    memset(&ipInfo, '0', sizeof(ipInfo));
+    getpeername(uniqueClient, (struct sockaddr *) &ipInfo, (unsigned int*)sizeof(ipInfo));
+    
+    // Assign FTP data port to Socket
+    ipInfo.sin_port = htons(dataport);
+    
+    // Connect data Socket to client
+    connect(dataSocket, (struct sockaddr *) &ipInfo, sizeof(ipInfo));
+    
+    // Return connected Socket
+    return dataSocket;
 };
 
 void listDirectory(int dataSocket){
     // http://stackoverflow.com/questions/30516546/how-are-dirent-entries-ordered
     DIR *directory = opendir(".");
-    struct dirent * entry;
+    struct dirent * dEntry;
     
-    while (entry == readdir(directory)){
-        send(
+    while ((dEntry = readdir(directory))){
+        
+        // Send a file
+        send(dataSocket, dEntry->d_name, strlen(dEntry->d_name), 0);
+        
+        // Send a new line
+        send(dataSocket, "\n", 1, 0);
     }
+    
+    closedir(directory);
            
 };
 
-void transferFile(){
+void transferFile(int dataSocket, char* command){
+    
+    char *temp = command;
+//    char *temp2;
+    char *filename;
+    
+    // Find the filename
+    temp = strchr(temp, ' ');
+    filename = temp++;
+    
+    
+    
     
 };
 

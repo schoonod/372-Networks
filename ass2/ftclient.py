624fd2ef12d764ea122f8047c8d7b83564069695
Dane Schoonover <Dane@Danes-iMac.local>
2016-03-05 17:02:04 -0800

    update

--- ass2/ftclient.py
+++ ass2/ftclient.py
@@ -22,41 +22,43 @@ def validateParameters(params):
 		print "Input correct number of arguments"
 		exit(1)
 
-	if len(params) = 4:
+	# if len(params) = 4:
 
-	if len(params) = 5:
+	# if len(params) = 5:
 
 # Start ftclient by specifying hostname/port
 def clientStart(serverName, serverPort):
 	controlSocket = socket(AF_INET, SOCK_STREAM)
-	controlSocket.connect(command, dataPort)
+	controlSocket.connect((serverName, int(serverPort)))
 	return (controlSocket)
 
 def printDirectory(controlSocket):
-	(dataSocket, address) = controlSocket.accept()
+	dataSocket, address = controlSocket.accept()
 	directory = dataSocket.recv(2048)
 	print directory
 	dataSocket.close()
 
 def receiveFile(controlSocket, fileName):
-	(dataSocket, address) = controlSocket.accept()
+	dataSocket, address = controlSocket.accept()
 	fileData = dataSocket.recv(2048)
 	file = open(fileName, 'w')
 	file.write(fileData)
 	dataSocket.close()
 	
 def dirRequest(controlSocket, command, dataPort):
-	controlSocket.send("%s %d") % (command, dataPort)
+	controlSocket.send(("%s %s") % (command, dataPort))
 	printDirectory(controlSocket)
 
 # Make request to host for file directory or file
 def fileRequest(controlSocket, command, fileName, dataPort):
-	controlSocket.send("%s %s %d") % (command, fileName, dataPort)
+	controlSocket.send(("%s %s %s") % (command, fileName, dataPort))
 	receiveFile(controlSocket, fileName)
 
 	
 if __name__ == '__main__':
 
+	print len(sys.argv)
+	print sys.argv[0]
 	validateParameters(sys.argv[1:])
 
 	serverName = sys.argv[1]
@@ -67,15 +69,20 @@ if __name__ == '__main__':
 
 	controlSocket = clientStart(serverName, serverPort)
 
-	command =	sys.argv[3]
+
+	command = sys.argv[3]
 	
-	if len(argv) < 5:
+	if len(sys.argv) == 5:
 		dataPort = sys.argv[4]
+		controlSocket.bind(('localhost', dataPort))
+		controlSocket.listen(0)
 		dirRequest(controlSocket, command, dataPort)
 
 	else:
 		fileName = sys.argv[4]
 		dataPort = sys.argv[5]
+		controlSocket.bind(('localhost', dataPort))
+		controlSocket.listen(0)
 		fileRequest(controlSocket, command, fileName, dataPort)
 
 	controlSocket.close()
